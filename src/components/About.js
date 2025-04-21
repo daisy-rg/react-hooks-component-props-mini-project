c88b94119c59c6f067dbad96c2340705a687aaba
daisy <chpdaisy@gmail.com>
2025-04-21 15:47:27 +0300

    putting them together is a done

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,9 @@
-function About({image,about}){ 
+function About({image="https://via.placeholder.com/215",about}){ 
     return(
     <aside>
-        <img src={image} alt=""/>
+        <img src={image} alt="blog logo"/>
         <p>{about}</p>
     </aside>
-    )
+    );
 }
 export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,15 @@ import React from "react";
 import blogData from "../data/blog";
 import Header from "./Header"
 import About from "./About";
+import ArticleList from "./ArticleList";
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
-      <div>
         <Header name={blogData.name}/>
         <About image={blogData.image} about={blogData.about} />
-      </div>
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+        <ArticleList posts={blogData.posts} />
     </div>
   );
 }
